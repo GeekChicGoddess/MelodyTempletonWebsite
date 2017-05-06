e93b6f851f4503d1dff3e033dcc4af8cedc2b84a
melodytempleton <mtempletoncantu@gmail.com>
2017-05-06 12:11:38 -0500

    improved magic transition of images to top left combined magic into a function all js is in the js file now

--- js/js.js
+++ js/js.js
@@ -3,6 +3,8 @@
  */
 $(document).ready(function(){
 
+
+    // JS FOR TOP SECTION _____________________________________________________________________________________
     $(window).scroll(function(){
 
         var wScroll = $(this).scrollTop();
@@ -13,16 +15,82 @@ $(document).ready(function(){
     });
 
 
-    // $("#weather").hover(function(){
-    //     // $("#weatherBanner").css({
-    //     //      'visibility' : 'show',
-    //     //     'z-index' : '2'
-    //     // });
-    // $("#weatherBanner").css("visibility", "show");
-    //
-    //
-    // });
+    // JS FOR PORTFOLIO SECTION __________________________________________________________________________________
+    var widthGrowAmount = 100;
+    var heightGrowAmount = 50;
+    var simonPage = "https://geekchicgoddess.github.io/simple-simon/";
+    var weatherPage = "https://geekchicgoddess.github.io/WeatherPredictionAPI/";
 
+    function shiftToTopLeft (id){ // thumbnail and sparkle move (animated) to top left corner before growth begins
+        var theElement = $(id);
+        var elementPosition = theElement.position();
+        var currentLeft = elementPosition.left;
+        var currentTop = elementPosition.top;
+        $(id).css( "position", "fixed");
+
+        var startInCorner = setInterval(function(){
+            if(currentLeft>0 || currentTop>0) {
+                currentTop-=10;
+                currentLeft-=10;
+                $(id).css({
+                    "top": currentTop,
+                    "left": currentLeft
+                });
+            }
+            else{ clearInterval(startInCorner); }
+        }, 25);
+    }
+
+
+    function grow(id){ // animate the growth of the thumbnail and the sparkle to fill the page
+        var sparkleGrowBig = setInterval(function(){
+            if(widthGrowAmount<1700) {
+                widthGrowAmount++;
+                heightGrowAmount++;
+                $(id).css({
+                    "width": widthGrowAmount + 100+ "px",
+                    "height": heightGrowAmount + 100 + "px"
+                });
+            }
+            else{ clearInterval(sparkleGrowBig); }
+        },5);
+    }
+
+    function moveRight(id) { // the sparkle needs to shift right to stay center in the "thumbnail" when it fills the screen
+        var rightMove = setTimeout(function(){
+            $(id).css("margin-left", "20%");
+
+        }, 1500);
+    }
+
+    function gotToProjectPage (href){ // load the actual project for the user to interact with
+       var projectPageRelocation = setTimeout(function(){
+            window.location.href = href;
+        }, 5000);
+    }
+
+    function makeTheMagicHappen (sparkleID, thumbnailID, address) {
+        $(sparkleID).css("display", "block");
+        shiftToTopLeft(sparkleID);
+        shiftToTopLeft(thumbnailID);
+        grow(sparkleID);
+        grow(thumbnailID);
+        moveRight(sparkleID);
+        gotToProjectPage(address);
+    }
+
+    $("#weather").click(function(){
+        makeTheMagicHappen("#weatherSparkle", "#weather", weatherPage);
+    });
+
+    $("#simon").click(function(){
+        makeTheMagicHappen("#simonSparkle", "#simon", simonPage);
+    });
+
+
+
+
+   // JS FOR RESUME SECTION ________________________________________________________________________________
     $('#profile a').click(function (e) {
         e.preventDefault();
         $(this).tab('show');
